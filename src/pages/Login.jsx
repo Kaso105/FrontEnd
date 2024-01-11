7acdbf399a0cd30e24df7cd83c051b55c474c389
Kaso105 <Kaso5.ponceleon@gmail.com>
2024-01-11 17:48:31 -0400

    firebase login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,25 @@ import { useNavigate } from 'react-router-dom';
 import { TextField, Button, CircularProgress } from "@mui/material";
 import { AuthContext } from '../contexts/auth/auth.context';
 import axios from 'axios';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithEmailAndPassword } from'firebase/auth';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBWQHmOEAu32foNykCmHI4OsiwP7RxGnVY",
+  authDomain: "winter-clone-408105.firebaseapp.com",
+  projectId: "winter-clone-408105",
+  storageBucket: "winter-clone-408105.appspot.com",
+  messagingSenderId: "972392293350",
+  appId: "1:972392293350:web:1f76198f6f2e0ec6966c27",
+  measurementId: "G-TT3GQ819Q1"
+};
+
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
 const Login = () => {
-
+  
   const authState = useContext(AuthContext);
 
   const [form, setForm] = useState({username: "", password: ""})
@@ -21,9 +37,11 @@ const Login = () => {
       setIsLoading(true);
 
       const response = await axios.get(`http://localhost:8080/cmcapp-backend-1.0/api/v1/admin/username/${form.username}`);
-
+      console.log("hola");
       const user = response.data.response;
-
+      console.log(user._username);
+      const credentials = await signInWithEmailAndPassword(auth, form.username, form.password);
+      console.log(credentials);
       if (form.username === user._username && form.password === user._password)
         authState.dispatch({type: "authenticated", payload: user});
       else
@@ -32,7 +50,7 @@ const Login = () => {
     }catch(error){
       console.log(error);
       alert("Error interno.");
-    } finally{
+    }finally{
       setIsLoading(false);
     }
   };
